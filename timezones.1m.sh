50eb4b4c8dac41cdf67617bc371afe34e139b68a
Rob Sonke <r.sonke@maxxton.com>
2016-08-09 11:15:19 +0200

    Added nvm support

--- timezones.1m.sh
+++ timezones.1m.sh
@@ -12,11 +12,14 @@
 #
 
 export PATH=${PATH}:/usr/local/bin
+# Nodejs version manager
+export NVM_DIR="$HOME/.nvm"
+source $(brew --prefix nvm)/nvm.sh
 
 getTime() {
   TZ="$2"
   MOMT="`dirname $0`/node_modules/moment-timezone"
-  
+
   RES=`echo "var m=require('${MOMT}'); console.log(m().tz('$2').format('h:mm A'))" | node`
   if [ ! -z "$3" ]; then
     echo "$1" "$RES" "| font='Inconsolata LGC'"
